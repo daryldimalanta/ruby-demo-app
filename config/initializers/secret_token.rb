5140d32d1ecf09ec549f3b81626ec89834f88ec0
Daryl Dimalanta <daryl@fluxifi.com>
2012-07-01 18:20:50 -0700

    created ch3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyDemoApp::Application.config.secret_token = 'b60c6dab18f6647aaff9636e4b38392679a876e2eb730ec42126ac1669373451e34504ed93f2ab0b61fb5fd6d68b4dda538d7dba9aebdf82ca746374eb29a938'
